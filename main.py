8c17a7aab709ea11aa9c53a35da070c78e3c70f9
Franco Colmenarez <francoacg1@gmail.com>
2020-04-19 17:30:45 -0500

    Drawing the cells

--- main.py
+++ main.py
@@ -10,5 +10,28 @@ screen = pygame.display.set_mode((width, height))
 bg = 25, 25, 25
 screen.fill(bg)
 
+# Number of cells
+nxC, nyC = 25, 25
+
+# Size of cells
+dimCW = width / nxC
+dimCH = height / nyC
+
 while True:
-    pass
+
+
+    for x in range(0, nxC):
+        for y in range(0, nyC):
+
+
+
+            # The square to draw
+            polygon = [((x)   * dimCW, y * dimCH),
+                       ((x+1) * dimCW, y * dimCH),
+                       ((x+1) * dimCW, (y+1) * dimCH),
+                       ((x)   * dimCW, (y+1) * dimCH)]
+
+            # Draw a square with 1 pixel of width
+            pygame.draw.polygon(screen, (128, 128, 128), polygon, 1)
+
+    pygame.display.flip()
