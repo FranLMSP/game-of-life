5ba4446efd76e23e129f88681fec55e085e3f495
Franco Colmenarez <francoacg1@gmail.com>
2020-04-19 17:57:07 -0500

    Game pausing functionality

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ dimCH = height / nyC
 gameState = np.zeros((nxC, nyC))
 newGameState = np.zeros((nxC, nyC))
 
+pause = False
 
 gameState[5, 3] = 1
 gameState[5, 4] = 1
@@ -34,28 +35,38 @@ while True:
 
     newGameState = np.copy(gameState)
 
+
+    # Register the event and mouse events
+    ev = pygame.event.get()
+    for event in ev:
+        if event.type == pygame.KEYDOWN:
+            pause = not pause
+
     for x in range(0, nxC):
         for y in range(0, nyC):
 
-            # Calculate tne neighbors
-            n_neigh = gameState[(x - 1) % nxC, (y - 1) % nyC] + \
-                      gameState[(x)     % nxC, (y - 1) % nyC] + \
-                      gameState[(x + 1) % nxC, (y - 1) % nyC] + \
-                      gameState[(x - 1) % nxC, (y)     % nyC] + \
-                      gameState[(x)     % nxC, (y)     % nyC] + \
-                      gameState[(x + 1) % nxC, (y)     % nyC] + \
-                      gameState[(x - 1) % nxC, (y + 1) % nyC] + \
-                      gameState[(x)     % nxC, (y + 1) % nyC] + \
-                      gameState[(x + 1) % nxC, (y + 1) % nyC]
+            # Don't update the game if it's paused
+            if not pause:
+
+                # Calculate tne neighbors
+                n_neigh = gameState[(x - 1) % nxC, (y - 1) % nyC] + \
+                          gameState[(x)     % nxC, (y - 1) % nyC] + \
+                          gameState[(x + 1) % nxC, (y - 1) % nyC] + \
+                          gameState[(x - 1) % nxC, (y)     % nyC] + \
+                          gameState[(x)     % nxC, (y)     % nyC] + \
+                          gameState[(x + 1) % nxC, (y)     % nyC] + \
+                          gameState[(x - 1) % nxC, (y + 1) % nyC] + \
+                          gameState[(x)     % nxC, (y + 1) % nyC] + \
+                          gameState[(x + 1) % nxC, (y + 1) % nyC]
 
 
-            # Rule 1: If a death cell has exacly 3 neighbors, the cell revives
-            if gameState[x, y] == 0 and n_neigh == 3:
-                newGameState[x, y] = 1
+                # Rule 1: If a death cell has exacly 3 neighbors, the cell revives
+                if gameState[x, y] == 0 and n_neigh == 3:
+                    newGameState[x, y] = 1
 
-            # Rule 2: If an alive cell has less than 2 neighbors and more than 3, the cell dies
-            elif gameState[x, y] == 1 and (n_neigh < 2 or n_neigh > 3):
-                newGameState[x, y] = 0
+                # Rule 2: If an alive cell has less than 2 neighbors and more than 3, the cell dies
+                elif gameState[x, y] == 1 and (n_neigh < 2 or n_neigh > 3):
+                    newGameState[x, y] = 0
 
             # The square to draw
             polygon = [((x)   * dimCW, y * dimCH),
