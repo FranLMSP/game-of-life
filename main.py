ca98a248006294a039b0869e45a19df54ca825ee
Franco Colmenarez <francoacg1@gmail.com>
2020-04-19 17:21:05 -0500

    Showing first screen

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+import numpy as np
+
+pygame.init()
+
+width, height = 1000, 1000
+screen = pygame.display.set_mode((width, height))
+
+# Background
+bg = 25, 25, 25
+screen.fill(bg)
+
+while True:
+    pass
